4f578245ba84447d968999e00452028a0ce75fdd
Gustavo Recio <gustavo.recio@mindcurv.com>
2021-03-22 15:16:45 +0100

    Adjsuted version

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source = "github.com/guxlightyear/terraform-aws-s3-bucket?ref=1.22.0"
+  source = "github.com/guxlightyear/terraform-aws-s3-bucket?ref=v1.22.0"
 
   bucket = "my-s3-bucket"
   acl    = "private"
