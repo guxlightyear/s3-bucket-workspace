6e7c6e470e81579881fccd556edbbda34e884a22
Gustavo Recio <gustavo.recio@mindcurv.com>
2021-03-23 10:56:23 +0100

    Enabled accleration status

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,10 @@ provider "aws" {
 }
 
 module "s3_bucket" {
-  source = "github.com/guxlightyear/terraform-aws-s3-bucket?ref=v1.22.0"
+  source = "github.com/guxlightyear/terraform-aws-s3-bucket?ref=v1.21.0"
+
+
+	acceleration_status = "Enabled"
 
   bucket = var.bucket_name
   acl    = "private"
